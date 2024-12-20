d6cb70e3d6f3b837fc64759413297b28fadcf432
Nicko Picazo <nicko.picazo@creatunity.com>
2024-12-20 19:36:45 +0800

    template 2

--- webpack.config.js
+++ webpack.config.js
@@ -8,60 +8,69 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HTMLInlineCSSWebpackPlugin =
   require("html-inline-css-webpack-plugin").default;
 
-module.exports = {
-  mode: "development",
-  entry: "./src/js/main.js",
-  output: {
-    filename: "js/main.js",
-    path: path.resolve(__dirname, "templates"),
-    clean: true,
-  },
-  devServer: {
-    static: path.resolve(__dirname, "templates"),
-    port: 3000,
-    hot: true,
-    liveReload: true,
-    watchFiles: ["src/*.html", "src/*.scss"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: false,
-      scriptLoading: "defer",
-      minify: true,
-      template: "src/template-1.html",
-      filename: "template-1.html",
-    }),
-    new miniCssExtractPlugin({
-      filename: "[name].css",
-    }),
-    new HTMLInlineCSSWebpackPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          miniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [require("tailwindcss"), require("autoprefixer")],
+module.exports = ({ mode }) => {
+  return {
+    mode: "development",
+    entry: "./src/js/main.js",
+    output: {
+      filename: "js/main.js",
+      path: path.resolve(__dirname, "templates"),
+      clean: true,
+    },
+    devServer: {
+      static: path.resolve(__dirname, "templates"),
+      port: 3000,
+      hot: true,
+      liveReload: true,
+      watchFiles: ["src/*.html", "src/*.scss"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: mode == "production" ? false : true,
+        scriptLoading: "defer",
+        minify: true,
+        template: "src/template-1.html",
+        filename: "template-1.html",
+      }),
+      new HtmlWebpackPlugin({
+        inject: mode == "production" ? false : true,
+        scriptLoading: "defer",
+        minify: true,
+        template: "src/template-2.html",
+        filename: "template-2.html",
+      }),
+      new miniCssExtractPlugin({
+        filename: "[name].css",
+      }),
+      mode != "production" && new HTMLInlineCSSWebpackPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            miniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [require("tailwindcss"), require("autoprefixer")],
+                },
               },
             },
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              api: "modern",
+            {
+              loader: "sass-loader",
+              options: {
+                api: "modern",
+              },
             },
-          },
-        ],
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [new CssMinimizerPlugin()],
-  },
+          ],
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [new CssMinimizerPlugin()],
+    },
+  };
 };
